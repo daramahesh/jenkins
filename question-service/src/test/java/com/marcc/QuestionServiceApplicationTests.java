ae2b9dffea5c9049ea783ffeeadd9d57f88e7f59
Mahesh101999 <mahesh.durgadara@Eidiko-india.com>
2023-09-06 15:28:55 +0530

    added

---
+++ question-service/src/test/java/com/marcc/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
